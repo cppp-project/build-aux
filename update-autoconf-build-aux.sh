125df9f18edf094d048074510acd3ed4b04b8b33
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2023-06-02 20:25:41 +0800

    Add autoconf build-aux update script.
    	新文件：   .gitignore
    	重命名：   ar-lib -> autoconf/ar-lib
    	重命名：   compile -> autoconf/compile
    	重命名：   config.guess -> autoconf/config.guess
    	重命名：   config.sub -> autoconf/config.sub
    	重命名：   install-sh -> autoconf/install-sh
    	重命名：   ltmain.sh -> autoconf/ltmain.sh
    	重命名：   missing -> autoconf/missing
    	重命名：   mkinstalldirs -> autoconf/mkinstalldirs
    	新文件：   update-autoconf-build-aux.sh

---
+++ update-autoconf-build-aux.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+# Update autoconf build-aux from automake.git
+#
+# This script requires curl program in the PATH.
+
+# Copyright (C) 2023 The C++ Plus Project.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Usage: ./update-autoconf-build-aux.sh
+# 
+
+FILE_LIST="ar-lib compile config.guess config.sub install-sh"
+
+update()
+{
+    echo "-- Updating $2..."
+    mv $3/$2 $3/$2.old
+    curl "https://git.savannah.gnu.org/gitweb/?p=$1;a=blob_plain;f=lib/$2;hb=HEAD" > $3/$2 2> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "-- $3/$2 Update failed."
+        mv $3/$2.old $3/$2
+        return 0
+    fi
+    diff --color $3/$2 $3/$2.old
+    rm $3/$2.old
+    chmod +x $3/$2
+}
+
+# Main
+echo Updating build-aux scripts...
+rm -f autoconf/*.old
+
+for i in $FILE_LIST
+do
+    update automake.git $i autoconf
+done
